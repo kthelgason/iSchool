f5abe4f8d20a585a62a2464b7f715fa9e95b351b
Björn Orri Sæmundsson <bjornorri@gmail.com>
2013-11-05 21:54:15 +0000

    Began working on a tab for Málið

---
+++ MyschoolChecker/SSVMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSVMenuViewController.m
+//  MyschoolChecker
+//
+//  Created by Björn Orri Sæmundsson on 05/11/13.
+//  Copyright (c) 2013 Kári Tristan Helgason. All rights reserved.
+//
+
+#import "SSVMenuViewController.h"
+
+@interface SSVMenuViewController ()
+
+@end
+
+@implementation SSVMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
